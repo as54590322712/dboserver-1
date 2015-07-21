34a101d025158b24212045e07f3c0f682a6fb9fc
snaity <snaity@gmail.com>
2015-07-21 19:14:55 -0300

    Update Database Files

--- Create User and Grant Acess.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'dboserver' IDENTIFIED BY 'dboserver';
-GRANT ALL ON dbo.* TO 'dboserver'@'%' IDENTIFIED BY 'dboserver';
-GRANT ALL ON dbo.* TO 'dboserver'@'localhost' IDENTIFIED BY 'dboserver';
-FLUSH PRIVILEGES;
